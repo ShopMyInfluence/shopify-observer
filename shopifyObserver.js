94302c36b42fee91d608e1964b2540c36be4c8dd
wassim-abderrazak <151826990+wassim-abderrazak@users.noreply.github.com>
2025-01-03 17:55:30 +0100

    Update shopifyObserver.js

--- shopifyObserver.js
+++ shopifyObserver.js
@@ -2,22 +2,21 @@
   const urlParams = new URLSearchParams(window.location.search);
   const smiclickId = urlParams.get('smiclickId');
   const currentUrl = window.location.href;
+  console.log("currentUrl",currentUrl);
 if(smiclickId){
-  localStorage.setItem('smiclickId', smiclickId);}
-  
+  localStorage.setItem('smiclickId', smiclickId);
+}
+  console.log("currentUrl.includes",currentUrl.includes("checkouts") && currentUrl.includes("thank-you"));
   // Vérifier si l'URL contient "checkout" et "thank-you"
-  if (currentUrl.includes("checkout") && currentUrl.includes("thank-you")) {
+  if (currentUrl.includes("checkouts") && currentUrl.includes("thank-you")) {
     // Extraire le token (cartId) depuis l'URL
     const cartId = currentUrl.split('/checkouts/')[1]?.split('/thank-you')[0];
+    console.log("cartId",cartId);
 
     if (!cartId) {
       console.error("Cart ID (token) not found in the URL.");
       return;
     }
-
-    // Stocker le smiclickId dans le localStorage
-    
-
     // Envoyer une requête à l'API avec le cartId
     fetch("https://smi--development.gadget.app/storeClickId", {
       method: "POST",
